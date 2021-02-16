dc6cb45d8689911b424b7b5971a129129c0f9412
yidan <ys303@duke.edu>
2021-02-16 11:06:56 +0800

    final

--- project2/my_malloc.h
+++ project2/my_malloc.h
@@ -9,14 +9,13 @@ struct __Node{
 
 typedef struct __Node Node;
 
+//Thread Safe malloc/free: locking version
 Node *call_sbrk(size_t size);
 void edit_lls(Node **temp,size_t size);
-//Thread Safe malloc/free: locking version
 void *ts_malloc_lock(size_t size);
 void ts_free_lock(void *ptr);
 
 //Thread Safe malloc/free: non-locking version
 Node *call_sbrk_nolock(size_t size);
-void edit_lls_nolock(Node **temp,size_t size);
 void *ts_malloc_nolock(size_t size);
 void ts_free_nolock(void *ptr);
